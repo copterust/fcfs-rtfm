07855d6a2e276b6d293046b8bf27288d0c7e5c89
Alexander Zhuravlev <123368+bofh@users.noreply.github.com>
2019-02-13 18:36:56 +0100

    Catch EXTI0

--- src/main.rs
+++ src/main.rs
@@ -14,34 +14,30 @@ const APP: () = {
     #[init]
     fn init() {
         let device: stm32f30x::Peripherals = device;
-        // Use PA5 for debug LED
-        device.RCC.ahbenr.modify(|_, w| w.iopaen().set_bit());
-        device.GPIOA.moder.modify(|_, w| w.moder5().output());
-        device.GPIOA.bsrr.write(|w| w.bs5().clear_bit());
-        // Use PC13 as INT source
+        // Use PA0 as INT source
         device.RCC.ahbenr.modify(|_, w| w.iopcen().set_bit());
-        device.GPIOC.moder.modify(|_, w| w.moder13().input());
+        device.GPIOA.moder.modify(|_, w| w.moder0().input());
         device
-            .GPIOC
+            .GPIOA
             .pupdr
-            .modify(|_, w| unsafe { w.pupdr13().bits(0b01) });
-        // Set PC13 as EXTI13
+            .modify(|_, w| unsafe { w.pupdr0().bits(0b01) });
+        // Set PCA0 as EXTI13
         device.RCC.apb2enr.write(|w| w.syscfgen().enabled());
         device
             .SYSCFG
-            .exticr4
-            .modify(|_, w| unsafe { w.exti13().bits(0b010) });
+            .exticr1
+            .modify(|_, w| unsafe { w.exti0().bits(0b000) });
         // Enable external interrupt on rise
-        device.EXTI.imr1.modify(|_, w| w.mr13().set_bit());
-        device.EXTI.emr1.modify(|_, w| w.mr13().set_bit());
-        device.EXTI.rtsr1.modify(|_, w| w.tr13().set_bit());
+        device.EXTI.imr1.modify(|_, w| w.mr0().set_bit());
+        device.EXTI.emr1.modify(|_, w| w.mr0().set_bit());
+        device.EXTI.rtsr1.modify(|_, w| w.tr0().set_bit());
         hprintln!("init!").unwrap();
         // Save device in resources for later use
         DEVICE = device;
     }
 
     #[interrupt(resources = [DEVICE])]
-    fn EXTI15_10() {
-        hprintln!("int15").unwrap();
+    fn EXTI0() {
+        hprintln!("EXTI0").unwrap();
     }
 };
