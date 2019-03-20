3c2664756c612680ef91e8f49fbaf13efd64933b
Alexander Zhuravlev <123368+bofh@users.noreply.github.com>
2019-03-20 19:44:08 +0100

    Draw linegraph

--- vis/realtime.py
+++ vis/realtime.py
@@ -3,26 +3,22 @@ import struct
 import visdom
 import numpy as np
 
-#port = serial.Serial("/dev/ttyUSB0", baudrate=115200, timeout=3.0)
+port = serial.Serial("/dev/ttyUSB0", baudrate=460800, timeout=3.0)
 viz = visdom.Visdom(port=8097, server="http://localhost")
-#viz.line(Y=np.random.rand(10),
-#         win="Yaw",
-#         name="Yaw")
-
-xs = []
-ys = []
+viz.line(X=np.array([0]),
+         Y=np.array([0]),
+         win="Yaw",
+         name="Yaw",
+         update='append')
 x = 0
-
 while True:
-#    rcv = port.read(10)
-#    print("Got bytes: {}".format(rcv))
-#    value = struct.unpack('d', rcv)[0]
-#    print("Value is: {}".format(value))
-    xs.append(x)
+    #rcv = port.read(4)
+    #print("Got bytes: {}".format(rcv))
+    #value = struct.unpack('d', rcv)[0]
+    #print("Value is: {}".format(value))
     x = x + 1
-    ys.append(np.random.rand(1))
-    viz.line(X=xs,
-             Y=ys,
+    viz.line(X=np.array([x]),
+             Y=np.random.rand(1),
              win="Yaw",
              name="Yaw",
              update='append')
