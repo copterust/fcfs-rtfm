bc829538ac5e0842d202877a9d975db6599d6362
Roma Sokolov <sokolov.r.v@gmail.com>
2019-05-05 20:40:54 +0200

    Bump alt-hal

--- src/types.rs
+++ src/types.rs
@@ -7,14 +7,14 @@ pub use hal::serial::Tx;
 pub use hal::spi::Spi;
 pub use hal::time::{Bps, Hertz};
 
-pub type SPI = Spi<hal::stm32f30x::SPI1,
+pub type SPI = Spi<hal::pac::SPI1,
                    (gpio::PB3<PullNone, AltFn<AF5, PushPull, HighSpeed>>,
                     gpio::PB4<PullNone, AltFn<AF5, PushPull, HighSpeed>>,
                     gpio::PB5<PullNone, AltFn<AF5, PushPull, HighSpeed>>)>;
 pub type Dev =
     mpu9250::SpiDevice<SPI, gpio::PB0<PullNone, Output<PushPull, LowSpeed>>>;
 pub type MPU9250 = mpu9250::Mpu9250<Dev, mpu9250::Imu>;
-pub type USART = stm32f30x::USART2;
+pub type USART = hal::pac::USART2;
 pub type TxUsart = Tx<USART>;
 pub type TxCh = dma1::C7;
 
@@ -22,8 +22,8 @@ pub type QuadMotors = (gpio::PA0<PullNone, gpio::Input>,
                        gpio::PA1<PullNone, gpio::Input>,
                        gpio::PA2<PullNone, gpio::Input>,
                        gpio::PA3<PullNone, gpio::Input>);
-pub type QuadMotorsTim = hal::stm32f30x::TIM2;
+pub type QuadMotorsTim = hal::pac::TIM2;
 
-pub type Add2MotorsTim = hal::stm32f30x::TIM3;
+pub type Add2MotorsTim = hal::pac::TIM3;
 pub type Add2Motors =
     (gpio::PA6<PullNone, gpio::Input>, gpio::PA7<PullNone, gpio::Input>);
