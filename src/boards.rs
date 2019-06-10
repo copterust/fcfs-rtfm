b33db9f227c7c41e2c31af7821bcc978a0bf8ece
Roma Sokolov <sokolov.r.v@gmail.com>
2019-06-10 23:12:06 +0200

    Add f3evo drone configuration

--- src/boards.rs
+++ src/boards.rs
@@ -34,7 +34,61 @@ pub struct InputDevice {
 }
 
 #[cfg(configuration = "configuration_drone")]
-mod defs {}
+mod defs {
+    use super::*;
+
+    pub type DebugPinDef<A, B> = gpio::PA11<A, B>;
+    type DT = DebugPinDef<PullNone, Input>;
+
+    pub type MpuIntPinDef<A, B> = gpio::PC13<A, B>;
+    type MT = MpuIntPinDef<PullNone, Input>;
+
+    pub type NcsPinDef<B> = gpio::PB9<PullNone, B>;
+    type NT = NcsPinDef<Input>;
+
+    pub type SpiT = hal::pac::SPI1;
+    pub type SCLPin<B> = gpio::PB3<PullNone, B>;
+    pub type MISOPin<B> = gpio::PB4<PullNone, B>;
+    pub type MOSIPin<B> = gpio::PB5<PullNone, B>;
+    pub type SpiInputPins = (SCLPin<Input>, MISOPin<Input>, MOSIPin<Input>);
+    pub type USART = hal::pac::USART2;
+    pub type UsartPins =
+        (gpio::PA14<PullNone, Input>, gpio::PA15<PullNone, Input>);
+    pub type TxUsart = Tx<USART>;
+    pub type TxCh = hal::dma::dma1::C7;
+    pub type ExtiNum = hal::exti::EXTI13;
+
+    type Res = BoardConfiguration<DT,
+                                  MT,
+                                  SpiT,
+                                  SpiInputPins,
+                                  NT,
+                                  USART,
+                                  UsartPins,
+                                  TxCh,
+                                  ExtiNum>;
+    pub fn configure(device: InputDevice,
+                     gpioa: gpio::Gpioa,
+                     gpiob: gpio::Gpiob,
+                     gpioc: gpio::Gpioc,
+                     ahb: &mut hal::rcc::AHB)
+                     -> Res {
+        let scl_sck = gpiob.pb3;
+        let ad0_sdo_miso = gpiob.pb4;
+        let sda_sdi_mosi = gpiob.pb5;
+        let dma_channels = device.DMA1.split(ahb);
+
+        BoardConfiguration { debug_pin: gpioa.pa11,
+                             mpu_interrupt_pin: gpioc.pc13,
+                             spi: device.SPI1,
+                             spi_pins: (scl_sck, ad0_sdo_miso, sda_sdi_mosi),
+                             ncs: gpiob.pb9,
+                             usart: device.USART2,
+                             usart_pins: (gpioa.pa14, gpioa.pa15),
+                             tx_ch: dma_channels.7,
+                             extih: device.EXTI.EXTI13 }
+    }
+}
 
 #[cfg(configuration = "configuration_dev")]
 mod defs {
